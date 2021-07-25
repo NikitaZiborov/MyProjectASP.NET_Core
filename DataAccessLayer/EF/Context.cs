45698bed7faa66c472ce929ab2047a37e8640972
Nikita ZIBOROV <Nikita.ZIBOROV@livenau.net>
2021-07-25 10:22:11 +0300

    Добавьте файлы проекта.

---
+++ DataAccessLayer/EF/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.EF
+{
+    public class Context : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Stadium> Stadiums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ComplexArchitectureDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ PresentationLayer/Controllers/GamesController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogic;
+using BusinessLogic.Entities;
+using CourseWork.Models;
+using AutoMapper;
+using CourseWork.ViewModels;
+
+namespace CourseWork.Controllers
+{
+    public class GamesController : Controller
+    {
+        public static int currentEditionID;
+
+        public IActionResult AddPage()
+        {
+            using (var db = new BL())
+            {
+                GameViewModel model = new GameViewModel()
+                {
+                    Players = Mapper.Map<List<PlayerModel>>(db.GetPlayers()),
+                    Stadiums = Mapper.Map<List<StadiumModel>>(db.GetStadiums())
+                };
+
+                return View(model);
+            }
+        }
+        public IActionResult AddGame(string date, int spectators, int place, int[] players, string result)
+        {
+            List<PlayerModel> chosenPlayers = new List<PlayerModel>();
+            StadiumModel chosenPlace = null;
+            Models.Result chosenResult = 0;
+
+            using (var db = new BL())
+            {
+                List<PlayerModel> listPlayers = Mapper.Map<List<PlayerModel>>(db.GetPlayers());
+
+                for (int i = 0; i < players.Length; i++)
+                {
+                    foreach (PlayerModel playerModel in listPlayers)
+                    {
+                        if (playerModel.Id == players[i])
+                        {
+                            chosenPlayers.Add(playerModel);
+                            break;
+                        }
+                    }
+                }
+
+                foreach (StadiumModel model in Mapper.Map<List<StadiumModel>>(db.GetStadiums()))
+                {
+                    if (model.Id == place)
+                    {
+                        chosenPlace = model;
+                        break;
+                    }
+                }
+
+                switch (result)
+                {
+                    case "Won":
+                        chosenResult = Models.Result.Won;
+                        break;
+
+                    case "Failed":
+                        chosenResult = Models.Result.Failed;
+                        break;
+
+                    case "Noone":
+                        chosenResult = Models.Result.Noone;
+                        break;
+
+                    case "NotPlayed":
+                        chosenResult = Models.Result.NotPlayed;
+                        break;
+                }
+
+                foreach (PlayerModel player in chosenPlayers)
+                {
+                    player.IsInGame = true;
+                    db.UpdatePlayer(Mapper.Map<PlayerBL>(player));
+                };
+
+                GameModel game = new GameModel()
+                {
+                    Players = chosenPlayers,
+                    Spectators = spectators,
+                    Date = date,
+                    Place = chosenPlace,
+                    GameResult = chosenResult,
+                };
+
+                db.AddGame(Mapper.Map<GameBL>(game));
+            }
+
+            return Redirect("~/Home/Index");
+        }
+
+        public IActionResult ShowAllPage()
+        {
+            List<GameModel> list = null;
+
+            using (var db = new BL())
+                list = Mapper.Map<List<GameModel>>(db.GetGames());
+
+            return View(list);
+        }
+    }
+}
